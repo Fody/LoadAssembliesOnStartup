8bc3f69ce16dfd2e679c39ca09381fa0423b7704
Geert van Horrik <geert@catenalogic.com>
2021-09-07 21:44:23 +0200

    Cache fast path results and file system queries

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Fody")]
 [assembly: AssemblyVersion("4.6.0")]
 [assembly: AssemblyFileVersion("4.6.0")]
-[assembly: AssemblyInformationalVersion("4.6.0-beta.1+1")]
+[assembly: AssemblyInformationalVersion("4.6.0-beta.5")]
 [assembly: AssemblyCopyright("Copyright © Fody 2015 - 2021")]
 
